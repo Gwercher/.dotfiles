f7fd00689780e6598dac908b410e6259f0c36fb3
Gwercher <gwerchersimon@gmail.com>
2024-07-16 22:24:42 +0200

    v2 installer scripts

--- 01_install.sh
+++ 01_install.sh
@@ -6,9 +6,6 @@ sudo apt install sudo wget git xorg -y
 # zsh
 sudo apt install zsh -y
 
-# oh my zsh
-cd ~/Downloads && sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-
 # polybar
 # sudo apt install build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev -y
 # sudo apt install libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev -y
@@ -23,3 +20,6 @@ sudo apt install firefox-esr -y
 
 #ranger
 sudo apt install ranger -y
+
+# oh my zsh
+cd ~/Downloads && sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
