e19d383b4910ca9d0cb283f019873fc5c79a4dd5
Gwercher <sigwercher@tsn.at>
2024-07-17 21:42:31 +0200

    added custom zsh plugins

--- 03_install_rust.sh
+++ 03_install_rust.sh
@@ -1,4 +1,4 @@
 # rust
 sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 desktop-file-utils -y
-# cd ~/Downloads && curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cd ~/Downloads && curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+omz reload
