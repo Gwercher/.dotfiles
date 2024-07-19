873e47f62df5c5b3341cb5d755179b0fe968cec7
Gwercher <sigwercher@tsn.at>
2024-07-19 13:47:55 +0200

    silent install changes

--- 01_install.sh
+++ 01_install.sh
@@ -6,25 +6,25 @@ sudo apt install sudo wget git xorg unzip -y
 # zsh
 sudo apt install zsh -y
 
+# oh my zsh
+cd ~/Downloads && sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" "" --unattended
+
 # polybar
-# sudo apt install build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev -y
-# sudo apt install libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev -y
-# cd ~/Downloads/ && git clone --recursive https://github.com/polybar/polybar && cd polybar && mkdir build && cd build && cmake .. && make -j$(nproc) && sudo make install
 sudo apt install polybar -y
 
 # i3
 sudo apt install i3 feh -y
 
-# firefox
-sudo apt install firefox-esr -y
+# firefox and thunderbird
+sudo apt install firefox-esr thunderbird -y
 
-#ranger
+# ranger
 sudo apt install ranger -y
+
+# sqlite3 and sqlitebrowser
+sudo apt install sqlite3 sqlitebrowser -y
 
 # font: dejavu sansm nerd font
 cd ~/Downloads && wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/DejaVuSansMono.zip && sudo unzip DejaVuSansMono.zip -d /usr/local/share/fonts
-
-# oh my zsh
-cd ~/Downloads && sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
 echo -e "\n\n\n\033[0;31mNEXT: 02_install_nvim.sh"
--- 03_install_rust.sh
+++ 03_install_rust.sh
@@ -2,6 +2,10 @@
 sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 desktop-file-utils -y
 cd ~/Downloads && curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
+#experimental
+source ~/.zshrc
+rustup override set stable && rustup update stable
+
 echo "\n\n\n"
 echo "\033[0;31mMANUAL RELOAD USING 'omz reload' NEEDED!"
 echo "\n\033[0;31mNEXT: 04_install_alacritty.sh"
