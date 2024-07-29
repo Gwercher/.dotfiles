a5bc4dff890f7879a5735f320169dee97f40ffad
Gwercher <sigwercher@tsn.at>
2024-07-29 15:44:36 +0200

    improved internet con check script

--- .config/polybar/internet-check.zsh
+++ .config/polybar/internet-check.zsh
@@ -1,9 +1,11 @@
 #!/bin/zsh
 
-if nc -zw1 google.com 443; then
+nc -z -w 5 8.8.8.8 53  >/dev/null 2>&1
+online=$?
+
+if [ $online -eq 0 ]; then
   echo "Online"
 else
-  echo $(date +%d-%m-%y\ %H:%M:%S) >> ~/internet-status/$(date +%m-%y)
-  # echo "%{F#e51515}Offline"
+# echo $(date +%d-%m-%y\ %H:%M:%S) >> ~/internet-status/$(date +%m-%y)
   echo "%{F#cc241d}Offline"
 fi
