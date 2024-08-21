abab774b83697d3c36eb7aed2a4a188fffa0ffce
Gwercher <sigwercher@tsn.at>
2024-08-21 18:04:06 +0200

    less polybar clutter

--- .config/polybar/internet-check.zsh
+++ .config/polybar/internet-check.zsh
@@ -4,7 +4,7 @@ nc -z -w 5 8.8.8.8 53  >/dev/null 2>&1
 online=$?
 
 if [ $online -eq 0 ]; then
-  echo "Online"
+  echo "Wifi"
 else
 # echo $(date +%d-%m-%y\ %H:%M:%S) >> ~/internet-status/$(date +%m-%y)
   echo "%{F#cc241d}Offline"
