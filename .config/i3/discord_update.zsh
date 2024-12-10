5671d9934c14f9a470f90a0f63b0f985ebc311e0
Gwercher <gwerchersimon@gmail.com>
2024-12-10 19:01:21 +0100

    improved discord update script

--- .config/i3/discord_update.zsh
+++ .config/i3/discord_update.zsh
@@ -9,9 +9,15 @@ URL="https://dl.discordapp.net/apps/linux/$new_version/discord-$new_version.deb"
 HTTP_STATUS=$(wget --spider --quiet --server-response $URL 2>&1 | grep "HTTP/" | tail -n 1 | awk '{print $2}')
 
 if [ "$HTTP_STATUS" -ne 200 ]; then
-    # echo "no new discord version"
+    echo "no new discord version"
     exit 0
 fi
 
-sudo dpkg -i discord-$new_version.deb
-rm discord-$new_version.deb
+OUTPUT_DIR=~/Downloads
+
+wget --quiet -P $OUTPUT_DIR $URL
+
+sudo dpkg -i $OUTPUT_DIR/discord-$new_version.deb > /dev/null 2>&1
+rm $OUTPUT_DIR/discord-$new_version.deb
+
+echo "updated discord"
