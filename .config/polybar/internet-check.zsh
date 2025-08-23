8145530529c550ed2932cae2473c6a43cad21d76
Gwercher <gwerchersimon@gmail.com>
2025-08-23 16:28:19 +0200

    added grayed out wifi if wifi is turned off

--- .config/polybar/internet-check.zsh
+++ .config/polybar/internet-check.zsh
@@ -1,11 +1,17 @@
 #!/bin/zsh
 
-nc -z -w 5 8.8.8.8 53  >/dev/null 2>&1
+wifi=$(nmcli radio wifi)
+
+if [[ "$wifi" == "disabled" ]]; then
+  echo "%{F#928374}Wifi"
+  exit
+fi
+
+nc -z -w 5 8.8.8.8 53 >/dev/null 2>&1
 online=$?
 
 if [ $online -eq 0 ]; then
   echo "Wifi"
 else
-# echo $(date +%d-%m-%y\ %H:%M:%S) >> ~/internet-status/$(date +%m-%y)
   echo "%{F#cc241d}Wifi"
 fi
