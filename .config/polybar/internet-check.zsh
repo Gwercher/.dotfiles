158aa002f677383b211855b5a22089863c11ba14
Gwercher <gwerchersimon@gmail.com>
2025-09-01 13:38:22 +0200

    improved internet check colors

--- .config/polybar/internet-check.zsh
+++ .config/polybar/internet-check.zsh
@@ -4,6 +4,7 @@ wifi=$(nmcli radio wifi)
 
 type="Wifi"
 con="󰖩"
+color=""
 
 if [[ "$wifi" == "disabled" ]]; then
   type=" Eth"
@@ -13,8 +14,9 @@ nc -z -w 5 8.8.8.8 53 >/dev/null 2>&1
 online=$?
 
 if [ $online -ne 0 ]; then
-  con="%{F#cc241d}󱚼"
+  con="󱚼"
+  color="%{F#cc241d}"
 fi
 
-echo $type $con
+echo $color$type $con
 
