db05b6d2684320b2c11e08af181b073ec83cf912
Gwercher <gwerchersimon@gmail.com>
2025-10-07 19:34:25 +0200

    updated overseer gcc task

--- .config/nvim/lua/overseer/template/user/gcc_build_run.lua
+++ .config/nvim/lua/overseer/template/user/gcc_build_run.lua
@@ -2,11 +2,13 @@ return {
   name = "gcc build and run",
   builder = function()
     local file = vim.fn.expand("%:p")
-    local outfile = vim.fn.expand("%:p:r") .. ".out"
+    local outfile = vim.fn.expand("%:p:r")
     return {
-      cmd = { outfile },
+      cmd = { "sh", "-c" },
+      args = {
+        string.format("gcc '%s' -o '%s' -Wall -Wextra -Werror -Wpedantic -std=c18 && '%s'", file, outfile, outfile),
+      },
       components = {
-        { "dependencies", task_names = { { cmd = "gcc", args = { file, "-o", outfile } } } },
         { "on_output_quickfix", open = true },
         "default",
       },
--- .config/nvim/lua/plugins/overseer.lua
+++ .config/nvim/lua/plugins/overseer.lua
@@ -4,7 +4,7 @@ return {
     opts = {
       templates = {
         "builtin",
-        "user.c_build",
+        "user.gcc_build_run",
       },
     },
   },
