beccf505b0a880c026eff5cf252d116510582a67
Gwercher <gwerchersimon@gmail.com>
2025-10-19 20:56:13 +0200

    updated nvim

---
+++ .config/nvim/after/ftplugin/tex.lua
@@ -0,0 +1,12 @@
+local Util = require("lazyvim.util")
+local map = Util.safe_keymap_set
+
+local file = vim.fn.expand("%:p")
+local outfile = vim.fn.expand("%:p:r")
+
+map(
+  "n",
+  "<leader>r",
+  string.format(":w | :TermExec cmd='pdflatex %s; :q' size=50 direction=float go_back=0<CR>", file),
+  { desc = "Build and Run Tex File" }
+)
