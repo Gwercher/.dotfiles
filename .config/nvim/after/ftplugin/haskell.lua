56b69acbb773c01d5b379d51d1679a5fb69fafb7
Gwercher <gwerchersimon@gmail.com>
2025-11-08 20:31:19 +0100

    another nvim config update

--- .config/nvim/after/ftplugin/haskell.lua
+++ .config/nvim/after/ftplugin/haskell.lua
@@ -7,4 +7,9 @@ map(
   ":w | :TermExec cmd='ghci %:p && :q' size=25 direction=float go_back=0<CR>",
   { desc = "Run Haskell (ghci)" }
 )
-map("n", "<leader>R", ":w | :TermExec cmd='cabal run' size=25 direction=float go_back=0<CR>", { desc = "cabal run" })
+map(
+  "n",
+  "<leader>R",
+  ":w | :TermExec cmd='cd %:p:h:h && cabal run' size=25 direction=float go_back=0<CR>",
+  { desc = "cabal run" }
+)
