c9c5b8e50a99a8a675b6a34286fb95f28ef5a54b
Gwercher <gwerchersimon@gmail.com>
2025-12-02 19:56:16 +0100

    added -g debug flag to c.lua

--- .config/nvim/after/ftplugin/c.lua
+++ .config/nvim/after/ftplugin/c.lua
@@ -4,6 +4,6 @@ local map = Util.safe_keymap_set
 map(
   "n",
   "<leader>r",
-  ":w | :TermExec cmd='gcc %:p -o %:p:r -Wall -Wextra -Werror -Wpedantic -std=c18 -Wno-unused-but-set-variable -Wno-unused-variable && %:p:r' size=25 direction=float go_back=0<CR>",
+  ":w | :TermExec cmd='gcc %:p -o %:p:r -g -Wall -Wextra -Werror -Wpedantic -std=c18 -Wno-unused-but-set-variable -Wno-unused-variable && %:p:r' size=25 direction=float go_back=0<CR>",
   { desc = "Build and Run C File" }
 )
