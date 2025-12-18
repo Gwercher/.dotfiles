3b9f8d2fa631850bdb34921e0acd07291516513e
Gwercher <gwerchersimon@gmail.com>
2025-12-18 19:34:41 +0100

    put files into /tmp instead of into ~/Downloads and then having to delete them

--- install_trixie.sh
+++ install_trixie.sh
@@ -10,8 +10,6 @@ PKG_ERROR_FILE=./PKG_$START_DATETIME.ERR
 DEB_PKGS_FILE=./DEB$REQ_DEBIAN_VER.pkgs
 LATEX_PKGS_FILE=./LATEX.pkgs
 
-DIR=$HOME/Downloads
-
 create_dir() {
 	mkdir -p $1 2>/dev/null || sudo mkdir -p $1 2>/dev/null
 }
@@ -44,7 +42,6 @@ main() {
 		exit 1
 	fi
 
-	create_dir $DIR
 	sudo apt-get update >/dev/null
 
 	echo -e "Installing packages via apt-get..."
@@ -61,37 +58,29 @@ main() {
 	echo -e "success installing apt-get packages!\n"
 
 	# oh my zsh
-	cd $DIR && sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" "" --unattended &&
-		sudo chsh -s /bin/zsh $USER
+	sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" "" --unattended && sudo chsh -s /bin/zsh $USER
 
 	# font: dejavu sansm nerd font
-	cd $DIR && wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/DejaVuSansMono.zip &&
-		sudo unzip DejaVuSansMono.zip -d /usr/local/share/fonts &&
-		rm $DIR/DejaVuSansMono.zip
+	wget -O /tmp/font.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/DejaVuSansMono.zip && sudo unzip /tmp/font.zip -d /usr/local/share/fonts
 
 	# neovim
-	cd $DIR &&
-		git clone https://github.com/neovim/neovim &&
-		cd neovim &&
+	git clone https://github.com/neovim/neovim /tmp/neovim &&
+		cd /tmp/neovim &&
 		git checkout stable &&
 		make CMAKE_BUILD_TYPE=RelWithDebInfo &&
 		cd build &&
 		cpack -G DEB
-
-	local nvim_deb=$(ls $DIR/neovim/build | grep '^nvim.*\.deb$')
-	sudo dpkg -i $DIR/neovim/build/$nvim_deb
-
-	rm $DIR/neovim -rf
+	local nvim_deb=$(ls /tmp/neovim/build | grep -E "^nvim.*.deb$")
+	sudo dpkg -i /tmp/neovim/build/$nvim_deb
 
 	# rust
-	cd $DIR && curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 	. "$HOME/.cargo/env"
 	rustup override set stable && rustup update stable
 
 	# alacritty
-	cd $DIR &&
-		git clone https://github.com/alacritty/alacritty.git &&
-		cd alacritty && cargo build --release --no-default-features --features=x11 &&
+	git clone https://github.com/alacritty/alacritty.git /tmp/alacritty &&
+		cd /tmp/alacritty && cargo build --release --no-default-features --features=x11 &&
 		sudo tic -xe alacritty,alacritty-direct extra/alacritty.info &&
 		sudo cp target/release/alacritty /usr/local/bin &&
 		sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg &&
@@ -101,23 +90,21 @@ main() {
 	create_dir /usr/local/share/man/man1
 	create_dir /usr/local/share/man/man5
 
-	scdoc <$DIR/alacritty/extra/man/alacritty.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty.1.gz >/dev/null
-	scdoc <$DIR/alacritty/extra/man/alacritty-msg.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz >/dev/null
-	scdoc <$DIR/alacritty/extra/man/alacritty.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty.5.gz >/dev/null
-	scdoc <$DIR/alacritty/extra/man/alacritty-bindings.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty-bindings.5.gz >/dev/null
+	scdoc </tmp/alacritty/extra/man/alacritty.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty.1.gz >/dev/null
+	scdoc </tmp/alacritty/extra/man/alacritty-msg.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz >/dev/null
+	scdoc </tmp/alacritty/extra/man/alacritty.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty.5.gz >/dev/null
+	scdoc </tmp/alacritty/extra/man/alacritty-bindings.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty-bindings.5.gz >/dev/null
 
 	create_dir ${ZDOTDIR:-~}/.zsh_functions
 	echo 'fpath+=${ZDOTDIR:-~}/.zsh_functions' >>${ZDOTDIR:-~}/.zshrc
 
-	cp $DIR/alacritty/extra/completions/_alacritty ${ZDOTDIR:-~}/.zsh_functions/_alacritty
-
-	rm $DIR/alacritty -rf
+	cp /tmp/alacritty/extra/completions/_alacritty ${ZDOTDIR:-~}/.zsh_functions/_alacritty
 
 	# change alacritty to default terminal
 	sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/alacritty 50
 
 	# install xidlehook
-	cd $DIR && cargo install xidlehook --bins
+	cargo install xidlehook --bins
 
 	# copy dotfiles
 	cp ~/.dotfiles/.config ~ -R
