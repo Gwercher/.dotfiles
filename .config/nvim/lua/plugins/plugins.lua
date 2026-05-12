056c7f1ed8af746f1c73f6dd061ba674c2190152
Gwercher <gwerchersimon@gmail.com>
2026-05-12 19:10:07 +0200

    added custom snippets support

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -24,10 +24,25 @@ return {
       },
     }),
   },
+
   {
     "neovim/nvim-lspconfig",
     opts = {
       vim.lsp.enable("julials"),
     },
+  },
+
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+    config = function(_, opts)
+      require("luasnip").setup(opts)
+
+      require("luasnip.loaders.from_vscode").lazy_load({
+        paths = { vim.fn.stdpath("config") .. "/snippets" },
+      })
+    end,
   },
 }
